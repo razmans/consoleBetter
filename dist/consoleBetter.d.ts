23fddb93118014ce7620830a7bf898060d105b7c
razmans <razmans@gmail.com>
2024-10-21 14:09:12 +0800

    Remove unnecessary TypeScript binaries and declaration files

--- dist/consoleBetter.d.ts
+++
@@ -1,8 +0,0 @@
-interface Console {
-    better: {
-        log: (message: string) => void;
-        toggleDebug: (enabled: boolean) => void;
-        trackPerformance: (callback: () => void) => void;
-    };
-    debugEnabled: boolean;
-}
