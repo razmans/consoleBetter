0b113adbd5f2f0674efc76583f29aa9206ab7749
razmans <razmans@gmail.com>
2024-10-21 14:18:00 +0800

    Refactor consoleBetter.ts to improve logging functionality

--- src/consoleBetter.ts
+++ src/consoleBetter.ts
@@ -9,6 +9,13 @@ declare global {
   }
 }
 
+/**
+ * Enum for log levels
+ * @readonly
+ * @enum {string}
+ * @property {string} DEBUG - Debug level
+ * @example LogLevel.DEBUG
+ */
 export enum LogLevel {
   DEBUG = 'DEBUG',
   INFO = 'INFO',
@@ -17,7 +24,7 @@ export enum LogLevel {
   FUNKY = 'FUNKY'
 }
 
-export const colorCodes: { [key in LogLevel]: string } = {
+const colorCodes: { [key in LogLevel]: string } = {
   [LogLevel.DEBUG]: '\x1b[34m', // Blue
   [LogLevel.INFO]: '\x1b[32m', // Green
   [LogLevel.WARN]: '\x1b[33m', // Yellow
@@ -27,6 +34,29 @@ export const colorCodes: { [key in LogLevel]: string } = {
 
 (console as any).debugEnabled = false; // Initialize debugEnabled
 
+/**
+ * * @module toggleDebug
+ * @param {boolean} enabled - Whether to enable debug mode
+ * @example `console.better.toggleDebug(true);` //to enable debug mode
+ * @example `console.better.toggleDebug(false);` //to disable debug mode
+ * By default, console.better.log will be disabled if toggleDebug==false
+ *
+ * @module log
+ * @param {string} message - The message to log
+ * @param {LogLevel} [level=LogLevel.INFO] - The log level
+ * @example `console.better.log('This is an informational message.', LogLevel.INFO);`
+ * @example `console.better.log('This is a warning message.', LogLevel.WARN);`
+ * @example `console.better.log('This is an error message.', LogLevel.ERROR);`
+ * @example `console.better.log('This is a funky message!', LogLevel.FUNKY);`
+ * @example `console.better.log('This is a debug message.', LogLevel.DEBUG);`
+ * @example `console.better.log('This is an informational message.');`
+ *
+ * @module trackPerformance
+ * @param {Function} callback - The function to track performance
+ * @example `console.better.trackPerformance(() => {
+ *  // Code to track performance
+ * })`;
+ */
 console.better = {
   log: (message: string, level: LogLevel = LogLevel.INFO) => {
     if (level === LogLevel.DEBUG && !console.debugEnabled) return;
