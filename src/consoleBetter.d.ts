15d8114b5865efe5a3012a1b8459b348a989c06f
razmans <razmans@gmail.com>
2024-10-21 18:46:04 +0800

    Update version in jsr.json to 1.0.1 and refactor import statements in README.md and consoleBetter.ts

---
+++ src/consoleBetter.d.ts
@@ -0,0 +1,8 @@
+declare module '@razmans/console-better' {
+  export class ConsoleBetter {
+    toggleDebug(): any; // Declare method with 'any' for now
+    trackPerformance(): any; // Declare method with 'any' for now
+  }
+
+  export const log: any; // Declare your log variable with 'any' type
+}
